74f1ed43d0a64ec37d41632c652f76c31ec42357
Moreen Hettihewa <moreenmadhushani>
2023-11-28 19:55:20 +0530

    added experince

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,12 +1,4 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { TimelineModule } from 'primeng/timeline';
-import { CardModule } from 'primeng/card';
-import {AvatarModule} from 'primeng/avatar';
-import {ImageModule} from 'primeng/image';
-import { ButtonModule } from 'primeng/button';
-import { DockModule } from 'primeng/dock';
-import { AccordionModule} from 'primeng/accordion'
 import { SharedComponentModule } from '../shared.module';
 
 export interface EventItem {
@@ -22,20 +14,53 @@ export interface EventItem {
   standalone: true,
   imports: [SharedComponentModule],
   templateUrl: './experience.component.html',
-  styleUrl: './experience.component.css'
+  styleUrl: './experience.component.css',
 })
 export class ExperienceComponent {
-
   events: EventItem[];
+
+  showRestfirst = false;
+  showRestSecond = false;
+  showRestThird = false;
 
   constructor() {
     this.events = [
-        { status: 'Ordered', date: '15/10/2020 10:30', icon: 'fa-shopping-cart', color: '#9C27B0', },
-        { status: 'Processing', date: '15/10/2020 14:00', icon: 'fa-cog', color: '#673AB7' },
-        { status: 'Shipped', date: '15/10/2020 16:15', icon: 'fa-shopping-cart', color: '#FF9800' },
-        { status: 'Delivered', date: '16/10/2020 10:00', icon: 'fa-check', color: '#607D8B' }
+      {
+        status: 'Ordered',
+        date: '15/10/2020 10:30',
+        icon: 'fa-shopping-cart',
+        color: '#9C27B0',
+      },
+      {
+        status: 'Processing',
+        date: '15/10/2020 14:00',
+        icon: 'fa-cog',
+        color: '#673AB7',
+      },
+      {
+        status: 'Shipped',
+        date: '15/10/2020 16:15',
+        icon: 'fa-shopping-cart',
+        color: '#FF9800',
+      },
+      {
+        status: 'Delivered',
+        date: '16/10/2020 10:00',
+        icon: 'fa-check',
+        color: '#607D8B',
+      },
     ];
-    
-}
+  }
 
+  onClickfirst() {
+    this.showRestfirst = !this.showRestfirst;
+  }
+
+  onClickSecond() {
+    this.showRestSecond = !this.showRestSecond;
+  }
+
+  onClickThird() {
+    this.showRestThird = !this.showRestThird;
+  }
 }
